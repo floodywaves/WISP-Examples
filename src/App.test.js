27061cabf9846c14055af436a351bc00ce77c701
Joyce Zou <jzou771@gmail.com>
2024-01-31 20:57:35 -0500

    my-app commit

--- src/App.test.js
+++ src/App.test.js
@@ -1,5 +1,6 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import NoteBoard from './noteboard';
 
 test('renders learn react link', () => {
   render(<App />);
---
+++ src/noteboard.js
@@ -0,0 +1,92 @@
+import React, { useState } from 'react';
+import './App.css';
+import Note from './note';
+const NoteBoard = (props) => {
+  //the useState allow functional components to have state
+  //const [state, setState] = useState(initialValue);
+    const [notes, setNotes] = useState([]); // The array returns with two element, notes and setNotes (ex. when setnotes is call, react will rerender)
+    const [noteID, setNoteID] = useState(0);
+    const [newNoteName, setNewNoteName] = useState('');
+    const [newNoteText, setNewNoteText] = useState('');
+   
+    const newNoteNameFunction = (event) => {
+        setNewNoteName(event.target.value);
+    }
+
+    const newNoteTextFunction = (event) => {
+        setNewNoteText(event.target.value);
+    }
+
+
+    const saveNoteInfo = () => {
+        // new syntax!
+        setNotes([...notes, { id: noteID, name: newNoteName, text: newNoteText }]);
+        
+        setNoteID((i) => i + 1)
+
+    }
+
+    // callback function
+    const deleteNote  = (id) => {
+        setNotes(notes.filter((i) => i.id !== id));
+    }
+
+    //callback function to change name and text of specific note
+    const editNote = (id, newName, newText) => {
+
+        // JS concept of IMMUTABILITY: need to create a copy of the array and set that as the 
+        // new note state, can't "mutate" the old state array in place!
+        const newNotes = notes.map((note, i) => {
+            if (i === id) {
+              // if matches the ID in question, modify the note!
+              return {id: id, name: newName, text: newText};
+            } else {
+              // The rest haven't changed
+              return note;
+            }
+          });
+
+          // finally, set the state to the new array with modified note!
+          setNotes(newNotes);
+    
+    }
+
+    
+    const allNotes = notes.map((note) => {
+        return (
+            <Note
+                name={note.name}
+                text={note.text}
+                id={note.id}
+                key={note.id}
+                delete={deleteNote} // passing callback function!
+                edit={editNote}
+            />
+            );
+        }
+    );
+
+    
+
+
+   return (
+     <div>
+       <p> this is the a note board! </p>
+       <p>add a new note below!</p>
+       <p>enter title:</p>
+        <input type="text" value={newNoteName} onChange={newNoteNameFunction} />
+
+        <p>enter text:</p>
+        <input type="text" value={newNoteText} onChange={newNoteTextFunction} />
+       <button onClick={saveNoteInfo}>Save!</button>
+
+        <div className="notes-flex">
+          {allNotes}
+        </div>
+       
+
+     </div>
+   );
+ }
+
+export default NoteBoard;
